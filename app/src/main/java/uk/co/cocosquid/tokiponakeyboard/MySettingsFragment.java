c58f1eb3b4e335606a611eac44f2d8d4f842086e
Timeo Pochin <timeo.sam.pochin@gmail.com>
2020-06-02 13:09:25 +0100

    Added basic settings functionality

---
+++ app/src/main/java/uk/co/cocosquid/tokiponakeyboard/MySettingsFragment.java
@@ -0,0 +1,43 @@
+package uk.co.cocosquid.tokiponakeyboard;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.inputmethod.InputMethodManager;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class MySettingsFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceClickListener {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings, rootKey);
+        init();
+    }
+
+    private void init() {
+        Preference openOnScreenKeyboardSetting = findPreference("open_on_screen_keyboard_setting");
+        assert openOnScreenKeyboardSetting != null;
+        openOnScreenKeyboardSetting.setOnPreferenceClickListener(this);
+
+        Preference selectInputMethod = findPreference("select_input_method");
+        assert selectInputMethod != null;
+        selectInputMethod.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        switch (preference.getKey()) {
+            case "open_on_screen_keyboard_setting":
+                startActivityForResult(new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS), 0);
+                break;
+            case "select_input_method":
+                InputMethodManager imeManager = (InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                assert imeManager != null;
+                imeManager.showInputMethodPicker();
+                break;
+        }
+        return true;
+    }
+}
--- app/src/main/java/uk/co/cocosquid/tokiponakeyboard/TokiPonaIMESettings.java
+++ app/src/main/java/uk/co/cocosquid/tokiponakeyboard/TokiPonaIMESettings.java
@@ -1,14 +1,40 @@
 package uk.co.cocosquid.tokiponakeyboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
 
 import android.os.Bundle;
 
-public class TokiPonaIMESettings extends AppCompatActivity {
+public class TokiPonaIMESettings extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, new MySettingsFragment())
+                .commit();
+    }
+
+    @Override
+    public boolean onPreferenceStartFragment(PreferenceFragmentCompat caller, Preference pref) {
+
+        // Instantiate the new Fragment
+        final Bundle args = pref.getExtras();
+        final Fragment fragment = getSupportFragmentManager().getFragmentFactory().instantiate(
+                getClassLoader(),
+                pref.getFragment());
+        fragment.setArguments(args);
+        fragment.setTargetFragment(caller, 0);
+
+        // Replace the existing Fragment with the new Fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, fragment)
+                .commit();
+        return true;
     }
 }
